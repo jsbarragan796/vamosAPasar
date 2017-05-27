8455bd5ecd7e2dd355090f1b15a53413feefec0c
juanSebastian <js.barragan796@uniandes.edu.co>
2017-05-27 08:46:37 -0500

    correccion nombre

--- src/punto2/Coalicion.java
+++ src/punto2/Coalicion.java
@@ -6,10 +6,15 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 
-public class Coaliciones {
+public class Coalicion {
+	
+	public Coalicion(){
+		
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		Coalicion coliacion=new Coalicion();
 
 
 		while (true) {
