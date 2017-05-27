ce25fc02d46ad54e5575da1579669d9bbfb440a0
Stephannie Jiménez <steff456@hotmail.com>
2017-05-27 17:21:53 -0500

    RotacionV2

---
+++ src/punto2/CoalicionV2.java
@@ -0,0 +1,19 @@
+package punto2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CoalicionV2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+		String line= bf.readLine();
+		while(line!=null){
+			int n= Integer.parseInt(line);
+			StringTokenizer st= new StringTokenizer(bf.readLine(), " ");
+			
+			line= bf.readLine();
+		}
+	}
+}
