ac19fb952b88ea90634319a84c017b6e415d066b
juanSebastian <js.barragan796@uniandes.edu.co>
2017-05-27 12:13:42 -0500

    punto 2: funciona, con una recursion, falta condicion de los decimales de salida. Creo que uso el principio de PROGRAMACIÓN DINAMICA de la lectura de clase 3 MÉTODOS GENERALES DE SOLUCIÓN DE PROBLEMAS

--- src/punto2/Coalicion.java
+++ src/punto2/Coalicion.java
@@ -3,19 +3,51 @@ package punto2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 
 public class Coalicion {
-	
-	public Coalicion(){
+
+	public Coalicion() {
+
+	}
+
+	private double calcularMaximaNomina(int[] p, double maxNomina, int sumaAcciones, int indiceSumado) {
+		System.out.println("max "+maxNomina+" suma acciones "+sumaAcciones +" indice sumado "+indiceSumado);
+		System.out.println(" ");
+		double maxTemp=maxNomina;
 		
+		if (sumaAcciones <= 50) {
+			int indice = indiceSumado - 1;
+			while (indice >= 0) {
+				System.out.println("iteracion "+p[indice] +" con el indice sumado "+p[indiceSumado]);
+				double respTemp=calcularMaximaNomina(p, maxNomina, sumaAcciones + p[indice], indice);
+				if(maxTemp<respTemp){
+					maxTemp=respTemp;
+				}
+				indice--;
+			}
+			if(indiceSumado-1>=0){
+			return calcularMaximaNomina(p, maxTemp, p[p.length - 1] + p[indiceSumado - 1], indiceSumado - 1);
+			}else{
+				return maxTemp;
+			}
+		} else{
+
+			double nomina = (p[p.length - 1] * 100) / sumaAcciones;
+			if (nomina > maxNomina) {
+				return nomina;
+			}
+			else{
+				return maxNomina;
+			}			
+			
+		}
+
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		Coalicion coliacion=new Coalicion();
-
+		Coalicion coliacio;
 
 		while (true) {
 			int n;
@@ -46,20 +78,20 @@ public class Coalicion {
 				int total = 0;
 				for (int j = 0; j < p_split.length; j++) {
 					p[j] = new Integer(p_split[j]);
-					total+=p[j];
+					total += p[j];
 				}
-				if(total!=100){
+				if (total != 100) {
 					throw new Exception("La suma de los porcentajes de las acciones no es representa el 100% ");
 				}
-			
 
 			} catch (NumberFormatException e) {
 				throw new Exception("El p_" + (i + 1) + " no es un entero para el socio " + i);
 			}
+			coliacio= new Coalicion();
+	
+			double resp= coliacio.calcularMaximaNomina(p, 0, p[p.length-1], p.length-1);
+			System.out.println("nomina maxima "+resp);
 			
-			
-			System.out.println(n);
-			System.out.println(Arrays.toString(p));
 
 		}
 	}
